c5b7abd6dffa58ea44a26529a7d60884fd5ee8a1
ethanelliot7 <ethanwaelliot@gmail.com>
2025-06-20 13:11:04 +1200

    added tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import reactSWC from '@vitejs/plugin-react-swc'; 
-import path from 'path';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactSWC()],
+  plugins: [reactSWC(), tailwindcss()],
    resolve: {
     alias: {
       // You can define multiple aliases here
