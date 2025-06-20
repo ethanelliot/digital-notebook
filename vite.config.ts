84f87dd94fd5f8750714d069a9c2b5f772e28a03
ethanelliot7 <ethanwaelliot@gmail.com>
2025-06-20 13:05:40 +1200

    Changed module aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import reactSWC from '@vitejs/plugin-react-swc'; 
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [reactSWC()],
+   resolve: {
+    alias: {
+      // You can define multiple aliases here
+      // For '@/', it usually points to your 'src' directory
+      '@': path.resolve(__dirname, './src'), // <--- Add this block
+    },
+  },
 })
