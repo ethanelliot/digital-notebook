ef81499157691cf089f24c8609255d04a9346ff6
ethanelliot7 <ethanwaelliot@gmail.com>
2025-07-06 16:20:19 +1200

    Clean: Removed unnecessary divs from calendar-week-view

--- src/components/dashboard/calendar/calendar-view-week.tsx
+++ src/components/dashboard/calendar/calendar-view-week.tsx
@@ -24,21 +24,18 @@ const CalendarWeekView: React.FC<CalendarWeekViewProps> = ({
   const { openDialog } = useDialog();
   const view: CalendarView = "week";
   return (
-    <div className="flex-1 grid grid-cols-7 grid-rows-[auto_1fr] sm:gap-2">
-      {daysOfWeek.map((day) => (
-        <div key={day} className="h-6 font-bold text-center">
-          {day}
-        </div>
-      ))}
-      {days.map((day) => {
-        const dayNotes = notesByDate.get(format(day, "yyyy-MM-dd")) || [];
-        return (
-          <div
-            className="flex flex-col justify-center items-center sm:gap-2 gap-y-2"
-            onClick={onCellClick}
-          >
+    <>
+      <div className="grid grid-cols-7 sm:gap-2 mb-2">
+        {daysOfWeek.map((day) => (
+          <div key={day} className="h-6 font-bold text-center">
+            {day}
+          </div>
+        ))}
+
+        {days.map((day) => (
+          <div className="flex items-center justify-center">
             <p
-              className={`h-6 w-6 flex items-center justify-center text-sm font-bold  ${
+              className={`h-6 w-6 flex items-center justify-center text-sm font-bold ${
                 isToday(day)
                   ? "bg-primary text-primary-foreground rounded-full"
                   : ""
@@ -46,37 +43,43 @@ const CalendarWeekView: React.FC<CalendarWeekViewProps> = ({
             >
               {format(day, "d")}
             </p>
+          </div>
+        ))}
+      </div>
 
+      <div className="flex-1 grid grid-cols-7 divide-x border rounded-md sm:gap-2 sm:border-0">
+        {days.map((day) => {
+          const dayNotes = notesByDate.get(format(day, "yyyy-MM-dd")) || [];
+          return (
             <div
               key={day.toString()}
-              className={"sm:p-2 w-full h-full border sm:rounded-md "}
+              onClick={onCellClick}
+              className="sm:p-2 sm:border sm:rounded-md"
             >
-              <div className="h-full overflow-y-auto hide-scrollbar">
-                <div className="w-full flex justify-end mb-2">
-                  <Button
-                    variant={"ghost"}
-                    size={"icon"}
-                    className="h-6 w-6 md:flex hidden"
-                    onClick={() => onCellAction(day)}
-                  >
-                    <Plus />
-                  </Button>
-                </div>
-                <CalendarNotesList
-                  onNoteClick={(note: Note) => {
-                    openDialog("noteForm", {
-                      note: note,
-                    });
-                  }}
-                  notes={dayNotes}
-                  view={view}
-                />
+              <div className="w-full flex justify-end mb-2">
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  className="h-6 w-6 md:flex hidden"
+                  onClick={() => onCellAction(day)}
+                >
+                  <Plus />
+                </Button>
               </div>
+              <CalendarNotesList
+                onNoteClick={(note: Note) => {
+                  openDialog("noteForm", {
+                    note: note,
+                  });
+                }}
+                notes={dayNotes}
+                view={view}
+              />
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
