3d608d2e83050885199c3a809b91f1fbf0e79a08
ethanelliot7 <ethanwaelliot@gmail.com>
2025-07-06 16:54:56 +1200

    Clean: calendar view component borders on small devices

--- src/components/dashboard/calendar/calendar-view-month.tsx
+++ src/components/dashboard/calendar/calendar-view-month.tsx
@@ -30,70 +30,77 @@ const CalendarMonthView: React.FC<CalendarMonthViewProps> = ({
   const month = days[Math.floor(days.length / 2)];
 
   return (
-    <div
-      className={`flex-1 sm:gap-2 grid grid-cols-7  ${
-        Math.floor(days.length / 7) == 5
-          ? "grid-rows-[2em_repeat(5,1fr)]"
-          : "grid-rows-[2em_repeat(6,1fr)]"
-      }`}
-    >
-      {daysOfWeek.map((day) => (
-        <div key={day} className="font-bold text-center h-2">
-          {day}
-        </div>
-      ))}
-      {days.map((day) => {
-        const dayNotes = notesByDate.get(format(day, "yyyy-MM-dd")) || [];
+    <>
+      <div className="grid grid-cols-7 sm:gap-2 mb-2">
+        {daysOfWeek.map((day) => (
+          <div key={day} className="font-bold text-center">
+            {day}
+          </div>
+        ))}
+      </div>
 
-        return (
-          <div
-            key={day.toString()}
-            onClick={() => onCellClick?.(day)}
-            className={`min-h-0 sm:aspect-square border sm:rounded-md ${
-              !isSameMonth(day, month) ? "opacity-30" : ""
-            }`}
-          >
-            <div className="flex flex-col w-full h-full ">
-              <div className="flex justify-between w-full sm:p-1">
-                <p
-                  className={`h-6 w-6 flex items-center justify-center text-sm font-bold ${
-                    isToday(day)
-                      ? "bg-primary text-primary-foreground md:rounded-full rounded-br-md"
-                      : ""
-                  }`}
-                >
-                  {format(day, "d")}
-                </p>
+      <div
+        className={`flex-1 sm:gap-2 grid grid-cols-7 border rounded-md overflow-hidden sm:border-0  ${
+          Math.floor(days.length / 7) == 5
+            ? "grid-rows-[repeat(5,1fr)]"
+            : "grid-rows-[repeat(6,1fr)]"
+        }`}
+      >
+        {days.map((day) => {
+          const dayNotes = notesByDate.get(format(day, "yyyy-MM-dd")) || [];
 
-                <Button
-                  variant={"ghost"}
-                  size={"icon"}
-                  className="h-6 w-6 md:flex hidden"
-                  onClick={() => {
-                    onCellAction(day);
-                  }}
-                >
-                  <Plus />
-                </Button>
-              </div>
-              <div className="flex-1 p-1 sm:p-2 overflow-y-auto hide-scrollbar">
-                <CalendarNotesList
-                  onNoteClick={(note: Note) => {
-                    if (!isMobile) {
-                      openDialog("noteForm", {
-                        note: note,
-                      });
-                    }
-                  }}
-                  notes={dayNotes}
-                  view={view}
-                />
+          return (
+            <div
+              key={day.toString()}
+              onClick={() => onCellClick?.(day)}
+              className="min-h-0 border sm:aspect-square sm:rounded-md"
+            >
+              <div
+                className={`flex flex-col w-full h-full ${
+                  !isSameMonth(day, month) ? "opacity-30" : ""
+                }`}
+              >
+                <div className="flex justify-between w-full sm:p-1">
+                  <p
+                    className={`h-6 w-6 flex items-center justify-center text-sm font-bold ${
+                      isToday(day)
+                        ? "bg-primary text-primary-foreground md:rounded-full rounded-br-md"
+                        : ""
+                    }`}
+                  >
+                    {format(day, "d")}
+                  </p>
+
+                  <Button
+                    variant={"ghost"}
+                    size={"icon"}
+                    className="h-6 w-6 md:flex hidden"
+                    onClick={() => {
+                      onCellAction(day);
+                    }}
+                  >
+                    <Plus />
+                  </Button>
+                </div>
+                <div className="flex-1 p-1 sm:p-2 overflow-y-auto hide-scrollbar">
+                  <CalendarNotesList
+                    onNoteClick={(note: Note) => {
+                      if (!isMobile) {
+                        openDialog("noteForm", {
+                          note: note,
+                        });
+                      }
+                    }}
+                    notes={dayNotes}
+                    view={view}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
